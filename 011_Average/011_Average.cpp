01da1ffb17b0aab0a64ce8d941f7a2ab018d8d68
JGE0211 <ke8650@gmail.com>
2024-09-04 14:26:19 +0900

    2주차 수업 내용

---
+++ 011_Average/011_Average.cpp
@@ -0,0 +1,20 @@
+﻿// 011_Average.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int math = 95;
+    int eng = 97;
+    int kor = 88;
+
+    int sum = math + eng + kor;
+    double avg = sum / 3;  //  정수/정수 -> 정수  (소수점 아래 숫자들이 사라짐!!)
+    //제대로 된 계산 값을 만드려면!!!
+    //방법 1. double avg = sum / 3.0;        <- 실수로 나눔
+    //방법 2. double avg =(double)sum / 3;   <- sum을 double로 바꿈
+    ////////////ㄴ> double avg =(double)(sum / 3 )     <---이렇게 되면 괄호 안의 sum/3 먼저 계산 뒤, 더블로 형을 바꾸게 됨!!! 그러니 괄호 잘 하기!!!
+    printf("평균 : %f\n", avg);
+}
+
---
+++ 013_ASCII2/013_ASCII2.cpp
@@ -0,0 +1,22 @@
+﻿// 013_ASCII2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    char c = '/';
+    
+    printf("%c %X %d\n", c, c, c);  //대문자로 쓰면 대문자로 나옴 but, d는 안 됨
+    printf("\n");
+
+    char d = '$';
+    printf("%c %X %d\n", d, d, d);
+    printf("%c 0x%X %d\n", d, d, d);
+    printf("\n");
+
+    int a = 72;
+    printf("%c 0x%X %d\n", a,a,a);
+}
+
+
